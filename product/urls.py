9203f261bdbff9b75b6edd2d2556a19a138cf252
nishad <nishadkoormath@gmail.com>
2025-09-15 11:58:56 +0530

    refactor: clean and optimize product app

--- product/urls.py
+++ product/urls.py
@@ -3,8 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('products/',views.products_list, name='products' ),
-    path('login/',views.login , name='login'),
+    path('details/<int:id>/', views.details, name='product_details'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
-    path('details/<int:id>/', views.details, name='details'),
 ]
